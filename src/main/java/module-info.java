06c314f54afd454f38aef32b3b079d3eee268932
Bastienlsg <95766791+Bastienlsg@users.noreply.github.com>
2023-03-17 22:42:03 +0100

    retrait de deux dépendances

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,7 @@
 module com.cleanbast.cleanbast {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.sun.jna.platform;
-    requires com.sun.jna;
     requires java.desktop;
-
 
     opens com.cleanbast to javafx.fxml;
     exports com.cleanbast;
