ba938f6fd8f6141148e44f60d8ee245710433753
Bastienlsg <95766791+Bastienlsg@users.noreply.github.com>
2023-03-18 18:46:37 +0100

    ajout des commentaires

--- src/main/java/com/cleanbast/CleanerController.java
+++ src/main/java/com/cleanbast/CleanerController.java
@@ -16,6 +16,7 @@ import java.util.Objects;
 public class CleanerController {
     private final TargetedDocument targetedDocument;
 
+    // Les quatre champs ci-dessous sont injectés par JavaFX grâce à l'annotation @FXML
     @FXML
     public ImageView principalImageView;
     @FXML
@@ -29,22 +30,22 @@ public class CleanerController {
         this.targetedDocument = new TargetedDocument(this);
     }
 
+    // Effectue des initialisations spécifiques à la vue
     @FXML
     public void initialize() {
         Image image = new Image(Objects.requireNonNull(CleanerController.class.getResourceAsStream("/assets/pictures/cleaner.jpg")));
         this.principalImageView.setImage(image);
     }
 
+    // Méthode appelée lors du clic sur le bouton "Supprimer" dans le FXML
     public void requestDeleteDocuments(ActionEvent e) {
         File entryPath = new File(this.entryPathField.getText());
+        // Traitement du dossier par TargetedDocument pour récupérer les documents supprimés
         ArrayList<File> deletedDocuments = this.targetedDocument.processForDocumentDeletion(entryPath);
         this.deletedDocumentsView.getItems().addAll(deletedDocuments);
     }
 
-    public void setNotValidDocumentLabel(String notValidMessage) {
-        this.notValidDocumentLabel.setText(notValidMessage);
-    }
-
+    // Méthode appelée lors du clic sur le bouton "dossier" dans le FXML
     public void openDirectoryChooser(ActionEvent e) {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle("Choisir un dossier");
@@ -52,5 +53,10 @@ public class CleanerController {
         if (!Objects.equals(selectedDirectory, "null")) {
             this.entryPathField.setText(selectedDirectory);
         }
+    }
+
+    // Méthode permettant de définir le message d'erreur à afficher sur notValidDocumentLabel
+    public void setNotValidDocumentLabel(String notValidMessage) {
+        this.notValidDocumentLabel.setText(notValidMessage);
     }
 }
--- src/main/java/com/cleanbast/TargetedDocument.java
+++ src/main/java/com/cleanbast/TargetedDocument.java
@@ -15,12 +15,14 @@ public class TargetedDocument {
     }
 
     public ArrayList<File> processForDocumentDeletion(File document) {
+        // Vérification que le document est valide et que l'utilisateur a les droits d'accès nécessaires
         if (validTargetedDocument(document) && rightsOnDocument(document)) {
             deleteEmptyDocuments(document);
         }
         return this.deletedDocuments;
     }
 
+    // Vérification qu'il s'agit bien d'un document valide au parcours d'une suppression
     public boolean validTargetedDocument(File document) {
         if (!document.exists() || !document.isDirectory() || !rightsOnDocument(document)) {
             this.cleanerController.setNotValidDocumentLabel("Veuillez choisir un dossier valide");
@@ -30,20 +32,25 @@ public class TargetedDocument {
             return true;
         }
     }
+
+    // Vérification que l'utilisateur a les droits nécessaires sur le document
     public boolean rightsOnDocument(File document) {
         return document.canWrite() && !document.getName().startsWith(".") && !document.isHidden();
     }
 
     public void deleteEmptyDocuments(File document) {
         for (File file : document.listFiles()) {
+            // Le fichier actuel est un dossier et l'utilisateur a les droits nécessaires ? On fait un appel récursif
             if (file.isDirectory() && rightsOnDocument(file)) {
                 deleteEmptyDocuments(file);
+                // Le fichier est vide et l'utilisateur a les droits nécessaires ? On le supprime
             } else if (file.length() == 0 && rightsOnDocument(file)) {
                 this.deletedDocuments.add(file);
                 getDesktop().moveToTrash(file);
             }
         }
 
+        // Le dossier donné est maintenant vide et l'utilisateur a les droits nécessaires ? On le supprime
         if (document.listFiles().length == 0 & rightsOnDocument(document)) {
             this.deletedDocuments.add(document);
             getDesktop().moveToTrash(document);
