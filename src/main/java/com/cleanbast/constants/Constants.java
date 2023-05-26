da998a9c5e20e502c4bb723dc27efd116e50364a
Bastienlsg <bastien.lassaigne@gmail.com>
2023-05-27 00:14:42 +0200

    ajout d'un oubli

--- src/main/java/com/cleanbast/constants/Constants.java
+++ src/main/java/com/cleanbast/constants/Constants.java
@@ -1,7 +1,7 @@
 package com.cleanbast.constants;
 
 public final class Constants {
-    public static final String FXML_PATH = "/com/cleanbast/vie/CleanerView.fxml";
+    public static final String FXML_PATH = "/com/cleanbast/view/CleanerView.fxml";
     public static final String ICON_PATH = "/assets/icon/icon-balai.png";
     public static final String CSS_PATH = "/assets/css/style.css";
     public static final String IMAGE_PATH = "/assets/pictures/cleaner.jpg";
